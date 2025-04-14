89eaeddaf82b8957738799f04d1ba8d5c2d3df9d
Catalina Álvarez <catalina.alvarezlasso@gmail.com>
2025-04-14 20:43:27 -0300

    Actualización en sección Filosofia

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+
+document.getElementById("navbar-container").innerHTML = `
+  <nav>
+    <ul>
+      <li><a href="index.html">Inicio</a></li>
+      <li><a href="filosofia.html">Filosofía</a></li>
+    </ul>
+  </nav>
+`;
