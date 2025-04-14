a49312fd07971993f7813724a09c6956237df05e
Catalina Álvarez <catalina.alvarezlasso@gmail.com>
2025-04-14 20:17:38 -0300

    Actualización con navbar y sección Filosofía

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
+
 function toggleMenu() {
-    const menu = document.getElementById('menu');
-    const blurBg = document.getElementById('blur-bg');
-    menu.classList.toggle('active');
-    blurBg.classList.toggle('active');
+  document.getElementById('menuOverlay').style.width = '100%';
 }
 
 function closeMenu() {
-    document.getElementById('menu').classList.remove('active');
-    document.getElementById('blur-bg').classList.remove('active');
-}
-
-function growLogo() {
-    const logo = document.querySelector('.logo');
-    logo.style.transform = 'scale(1.17)';
-    setTimeout(() => {
-        logo.style.transform = 'scale(1)';
-    }, 1000);
+  document.getElementById('menuOverlay').style.width = '0';
 }
