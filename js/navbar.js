b3e0a9f30dd956fc7869e6b1a3b65aecd98a2c40
Catalina Álvarez <catalina.alvarezlasso@gmail.com>
2025-04-14 23:02:50 -0300

    Implementar Navbar y sección Inicio

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("navbar-container").innerHTML = `
-  <nav>
-    <ul>
-      <li><a href="index.html">Inicio</a></li>
-      <li><a href="filosofia.html">Filosofía</a></li>
-    </ul>
-  </nav>
-`;
