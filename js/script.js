cda2f69928faa8a907379c77534848512e34c7a3
Catalina Álvarez <catalina.alvarezlasso@gmail.com>
2025-04-15 15:50:36 -0300

    Actualizo styles.css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  const background = document.getElementById("background");
+  menu.classList.toggle("active");
+  background.classList.toggle("blur");
+}
