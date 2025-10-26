611a7f2f5c01495f02ae8052fa6ff9770f02118b
Catalina Álvarez <catalina.alvarezlasso@gmail.com>
2025-10-26 17:17:36 -0300

    Actualizacion masiva

--- js/scripts.js
+++ js/scripts.js
@@ -334,3 +334,15 @@ document.addEventListener('scroll', () => {
 })();
 
 
+
+
+if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+  function setViewportHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  setViewportHeight();
+  window.addEventListener('resize', setViewportHeight);
+  window.addEventListener('orientationchange', setViewportHeight);
+}
+
